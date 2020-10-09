2ebca089be1f4700e9509fee7ac03a0e75b92d6a
Lilian.Pacaud <Lilian.Pacaud@sky-real.com>
2020-10-09 16:36:24 +0200

    Add arrayMaximalAdjacentDifference

---
+++ arrayMaximalAdjacentDifference.py
@@ -0,0 +1,7 @@
+def arrayMaximalAdjacentDifference(inputArray):
+    max = 0
+    for i in range(len(inputArray)):
+        if i != len(inputArray) - 1:
+            if abs(inputArray[i] - inputArray[i+1]) > max:
+                max = abs(inputArray[i] - inputArray[i+1])
+    return max
